73e91befcc4ee482639b95fb0d0209f1bbb6d271
ReturnReason <reasonz@daum.net>
2022-12-14 10:38:21 +0900

    feat(OutputView): 에러 메시지를 출력하는 메서드 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,3 +1,4 @@
+const { ERROR_MESSAGE } = require('../constants/errorMessage');
 const { print, close } = require('../utils/mission');
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -19,6 +20,10 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult() {},
+
+  printError(errorType) {
+    print(ERROR_MESSAGE[errorType]);
+  },
 };
 
 module.exports = OutputView;
