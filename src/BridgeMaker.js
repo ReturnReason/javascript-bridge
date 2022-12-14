7321b98f4d44a6f6e2750e2d584670f431847988
ReturnReason <reasonz@daum.net>
2022-12-14 10:00:23 +0900

    refactor(BridgeMaker): 하드코딩으로 작성된 부분 게임 커맨드 상수로 변경

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -1,3 +1,4 @@
+const { GAME_COMMAND } = require('./constants/bridgeGame');
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
@@ -12,7 +13,7 @@ const BridgeMaker = {
       .fill()
       .map(() => {
         const number = generateRandomNumber();
-        return number ? 'U' : 'D';
+        return number ? GAME_COMMAND.up : GAME_COMMAND.down;
       });
   },
 };
