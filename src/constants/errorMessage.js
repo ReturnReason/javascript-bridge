bca034834a26e5117382dbf2359721f44f6fbffa
ReturnReason <reasonz@daum.net>
2022-12-14 10:23:03 +0900

    feat(errorMessage): 에러 메시지 상수 추가

---
+++ src/constants/errorMessage.js
@@ -0,0 +1,9 @@
+const ERROR_MESSAGE = {
+  outOfRange: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
+  emptyInput: '[ERROR] 공백 또는 빈 값은 입력할 수 없습니다.',
+  invalidNumber: '[ERROR] 숫자가 아닌 값은 입력할 수 없습니다.',
+  invalidMoveCommand: '[ERROR] 잘못된 커맨드입니다. U또는 D만 입력할 수 있습니다.',
+  invalidRetryCommand: '[ERROR] 잘못된 커맨드입니다. R또는 Q만 입력할 수 있습니다.',
+};
+
+module.exports = { ERROR_MESSAGE };
