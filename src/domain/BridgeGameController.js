4ef8de3b4daff57a412d35262dcf8caa94c6d401
ReturnReason <reasonz@daum.net>
2022-12-14 10:40:30 +0900

    feat(BridgeGameController): 사용자로부터 다리 길이를 입력받을 때 try catch 구문 추가

--- src/domain/BridgeGameController.js
+++ src/domain/BridgeGameController.js
@@ -2,9 +2,14 @@ const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
 const { GAME_MESSAGE } = require('../constants/gameMessage');
 const bridgeGameMap = require('../model/bridgeGameMap');
+const InputValidator = require('../validator/InputValidator');
 
 class BridgeGameController {
   #bridgeMap;
+
+  constructor() {
+    this.inputValidator = new InputValidator();
+  }
 
   start() {
     OutputView.printStart(GAME_MESSAGE.start);
@@ -13,7 +18,13 @@ class BridgeGameController {
 
   getBridgeSize() {
     const userInput = (input) => {
-      this.makeBridgeMap(input);
+      try {
+        this.inputValidator.checkValidBridgeSize(input);
+        this.makeBridgeMap(input);
+      } catch (errorType) {
+        OutputView.printError(errorType);
+        this.getBridgeSize();
+      }
     };
 
     InputView.readBridgeSize(userInput);
