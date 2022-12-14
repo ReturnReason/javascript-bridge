20b06481759044c6fd611c0d97112193cec67c57
ReturnReason <reasonz@daum.net>
2022-12-14 21:33:24 +0900

    feat(BridgeGameController): 사용자가 입력한 커맨드가 이동할 다리와 일치하는 경우 수행할 로직 추가

--- src/domain/BridgeGameController.js
+++ src/domain/BridgeGameController.js
@@ -3,12 +3,15 @@ const OutputView = require('../view/OutputView');
 const { GAME_MESSAGE } = require('../constants/gameMessage');
 const bridgeGameMap = require('../model/bridgeGameMap');
 const InputValidator = require('../validator/InputValidator');
+const User = require('../model/User');
 
 class BridgeGameController {
   #bridgeMap;
+  #user;
 
   constructor() {
     this.inputValidator = new InputValidator();
+    this.#user = new User();
   }
 
   start() {
@@ -21,6 +24,7 @@ class BridgeGameController {
       try {
         this.inputValidator.checkValidBridgeSize(input);
         this.makeBridgeMap(input);
+        this.getMoveCommand();
       } catch (errorType) {
         OutputView.printError(errorType);
         this.getBridgeSize();
@@ -32,6 +36,25 @@ class BridgeGameController {
 
   makeBridgeMap(size) {
     this.#bridgeMap = new bridgeGameMap(size);
+  }
+
+  getMoveCommand() {
+    const userInput = (input) => {
+      try {
+        const isCorrect = this.#bridgeMap.isCorrectBridge(input, this.#user.getLocation());
+
+        if (isCorrect) {
+          this.#user.increaseLocation();
+          this.getMoveCommand();
+        } else {
+          // 올바르지 않은 다리 선택시 리트라이로
+        }
+      } catch (errorType) {
+        OutputView.printError(errorType);
+      }
+    };
+
+    InputView.readMoving(userInput);
   }
 }
 
