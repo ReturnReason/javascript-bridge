f64b7876347cec09cae5d4b45ba512d7a06270d4
ReturnReason <reasonz@daum.net>
2022-12-16 22:10:20 +0900

    feat(InputValidator): 사용자가 입력한 이동 커맨드가 대문자 U 또는 D가 아닌 경우 예외처리하는 메서드 추가

--- src/validator/InputValidator.js
+++ src/validator/InputValidator.js
@@ -1,19 +1,31 @@
-const { GAME_INFO } = require('../constants/bridgeGame');
+const { GAME_INFO, GAME_COMMAND } = require('../constants/bridgeGame');
 const { ERROR_MESSAGE } = require('../constants/messages');
 
 class InputValidator {
   checkValidSize(input) {
     if (input < GAME_INFO.minSize || input > GAME_INFO.maxSize) throw ERROR_MESSAGE.invalidSize;
+
     return true;
   }
 
   checkEmpty(input) {
     if (!input || input.includes(' ')) throw ERROR_MESSAGE.emptyInput;
+
     return true;
   }
 
   checkIsNumber(input) {
     if (Number.isNaN(Number(input)) || input.includes('.')) throw ERROR_MESSAGE.notANumber;
+
+    return true;
+  }
+
+  checkMoveCommand(input) {
+    const regExp = /^U{1}$|^D{1}$/;
+    if (!regExp.test(input)) {
+      throw ERROR_MESSAGE.invalidMoveCommand;
+    }
+
     return true;
   }
 }
