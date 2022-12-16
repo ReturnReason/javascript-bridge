3a0b8e458b7565f56d28abc859ae05f3fc01d5ec
ReturnReason <reasonz@daum.net>
2022-12-16 21:11:29 +0900

    feat: 사용자로부터 다리의 길이를 입력받는 기능 구현

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -2,6 +2,16 @@ const OUTPUT_MESSAGE = Object.freeze({
   start: '다리 건너기 게임을 시작합니다.\n',
 });
 
+const INPUT_MESSAGE = Object.freeze({
+  bridgeSize: '다리의 길이를 입력해주세요.\n',
+});
+
+const ERROR_MESSAGE = Object.freeze({
+  invalidSize: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.\n',
+});
+
 module.exports = {
   OUTPUT_MESSAGE,
+  INPUT_MESSAGE,
+  ERROR_MESSAGE,
 };
--- src/domain/BridgeGameController.js
+++ src/domain/BridgeGameController.js
@@ -1,10 +1,26 @@
+const InputValidator = require('../validator/InputValidator');
+const InputView = require('../view/InputView');
 const OutputView = require('../view/OutputView');
 
 class BridgeGameController {
-  constructor() {}
+  constructor() {
+    this.inputValidator = new InputValidator();
+  }
 
   start() {
     OutputView.printStart();
+    this.getBridgeSize();
+  }
+
+  getBridgeSize() {
+    const callback = (input) => {
+      try {
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    InputView.readBridgeSize(callback);
   }
 }
 
